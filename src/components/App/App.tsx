71a03aab16eced756505fa0e780a5a1f07c50799
Nickolai <lindeman072@mail.ru>
2024-02-21 00:41:01 +0400

    fix: routs

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,36 +1,36 @@
+import React from 'react';
 import "./App.css";
-import Main from "../Main/Main";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Main from '../Main/Main';
+import AuthForm from '../AuthForm/AuthForm';
+
+const AppRouter: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<AuthForm />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
+
+// import "./App.css";
+// import Main from "../Main/Main";
 // import AuthForm from "../AuthForm/AuthForm";
 
-function App() {
-  return (
-    <>
-      <Main />
-      {/* <AuthForm /> */}
-    </>
-  );
-}
-
-export default App;
-
-
-
-
-// import React from 'react';
-// import "./App.css";
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Main from '../Main/Main';
-// import AuthForm from '../AuthForm/AuthForm';
-
-// const AppRouter: React.FC = () => {
+// function App() {
 //   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Main />} />
-//         <Route path="/login" element={<AuthForm />} />
-//       </Routes>
-//     </Router>
+//     <>
+//       <Main />
+//       <AuthForm />
+//     </>
 //   );
-// };
+// }
 
-// export default AppRouter;
+// export default App;
+
+
+
