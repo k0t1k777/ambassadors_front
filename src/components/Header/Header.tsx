dbcfe710f9020fd3eb45be5688b5a47341396f4c
Nickolai <lindeman072@mail.ru>
2024-02-21 14:57:20 +0400

    add: content

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,8 +3,15 @@ import Bell from "../../assets/Bell.svg";
 import Quest from "../../assets/Quest.svg";
 import Settings from "../../assets/Settings.svg";
 import Title from "../Title/Title";
+import { useLocation } from "react-router-dom";
 
 export default function Header() {
+  const location = useLocation();
+  // Проверяем, равен ли текущий путь "/login"
+  if (location.pathname === "/login") {
+    return null; // Если текущий путь "/login", не отображаем Header
+  }
+  
   return (
     <div className="header">
       <Title />
