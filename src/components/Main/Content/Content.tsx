251fb5366eb4ef783917da5e59dc70fc85e8b575
Olga Pankrashina <olyaolya2713@yandex.ru>
2024-02-22 17:09:31 +0300

    style: add icons and count card

--- src/components/Main/Content/Content.tsx
+++ src/components/Main/Content/Content.tsx
@@ -6,6 +6,11 @@ import ContentSortWindow from '../ContentSortWindow/ContentSortWindow';
 import ContentCard from '../ContentCard/ContentCard';
 
 export default function Content() {
+  const ContentData = {
+    new: 'Новенькие',
+    inProcess: 'В процессе',
+    done: 'Выполнено'
+  };
   return (
     <>
       <Header title="Контент" />
@@ -18,25 +23,60 @@ export default function Content() {
           <p>Filter</p>
         </div>
         <div className="content__grid">
-          <ContentSortWindow width="325px" height="75px" borderRadius="13px">
-            <p className="content__title">Новенькие</p>
-          </ContentSortWindow>
-          <ContentSortWindow width="451px" height="75px" borderRadius="13px">
-            <p className="content__title">В процессе</p>
-          </ContentSortWindow>
+          <div>
+            <ContentSortWindow width="325px" height="75px" borderRadius="13px">
+              <p className="content__title">{ContentData.new}</p>
+            </ContentSortWindow>
+            <ContentCard
+              tag="Соц сети"
+              name="Имя амбассадора"
+              social="Ник в телеграмм"
+              link="Сcылка на контент"
+              date="Aug 20, 2021"
+              count="0/4"
+              width="328px"
+              height="258px"
+            />
+            <ContentCard
+              tag="Соц сети"
+              name="Имя амбассадора"
+              social="Ник в телеграмм"
+              link="Сcылка на контент"
+              date="Aug 20, 2021"
+              count="0/4"
+              width="328px"
+              height="258px"
+            />
+            <ContentCard
+              tag="Соц сети"
+              name="Имя амбассадора"
+              social="Ник в телеграмм"
+              link="Сcылка на контент"
+              date="Aug 20, 2021"
+              count="0/4"
+              width="328px"
+              height="258px"
+            />
+          </div>
+          <div>
+            <ContentSortWindow width="451px" height="75px" borderRadius="13px">
+              <p className="content__title">{ContentData.inProcess}</p>
+            </ContentSortWindow>
+            <ContentCard
+              tag={['Хабр', 'Телеграмм', 'Хабр', 'Телеграмм']}
+              name="Имя амбассадора"
+              social="Ник в телеграмм"
+              link="Ссылка на контент"
+              date="Aug 20, 2021"
+              count="3/4"
+              width="451px"
+              height="258px"
+            />
+          </div>
           <ContentSortWindow width="446px" height="75px" borderRadius="13px">
-            <p className="content__title">Выполнено</p>
+            <p className="content__title">{ContentData.done}</p>
           </ContentSortWindow>
         </div>
-
-        <ContentCard
-          tag="Соц сети"
-          name="Имя амбассадора"
-          social="Ник в телеграмм"
-          link="Сcылка на контент"
-          date="Aug 20, 2021"
-          count="0/4"
-        />
       </section>
     </>
   );
--- src/components/Main/ContentCard/ContentCard.tsx
+++ src/components/Main/ContentCard/ContentCard.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
 import { Card, CardContent, Typography, CardMedia } from '@mui/material';
 import './ContentCard.css';
 import IconDone from '../../../assets/ContentIconDone.svg';
+import IconDoneOrange from '../../../assets/ContentIconDoneOrange.svg';
+import IconDoneGreen from '../../../assets/ContentIconDoneGreen.svg';
 
 interface ContentCardProps {
-  tag: string;
+  tag: string | string[];
   name: string;
   social: string;
   link: string;
   date: string;
   count: string;
+  width?: string;
+  height?: string;
 }
 
-export default function ContentCard({ tag, name, social, link, date, count }: ContentCardProps) {
-  const doneIcon = IconDone;
+export default function ContentCard({
+  tag,
+  name,
+  social,
+  link,
+  date,
+  count,
+  width = '451px',
+  height = '258px'
+}: ContentCardProps) {
+  let doneIcon: string;
+  const orangeDoneIcon = IconDoneOrange;
+  const greenDoneIcon = IconDoneGreen;
+
+  let countColorClass = '';
+  if (count === '2/4' || count === '3/4') {
+    countColorClass = 'card__orange-text';
+    doneIcon = orangeDoneIcon;
+  } else if (count === '4/4') {
+    countColorClass = 'card__green-text';
+    doneIcon = greenDoneIcon;
+  } else {
+    doneIcon = IconDone;
+  }
 
   return (
-    <Card className="card">
-      <CardContent className="card__content">
-        <Typography className="card__tag">{tag}</Typography>
+    <Card
+      className="card"
+      style={{ width, height }}
+      sx={{ boxShadow: 'none', borderRadius: '17px' }}
+    >
+      <CardContent sx={{ padding: '22px  25px 19px 25px' }}>
+        {typeof tag === 'string' ? (
+          <Typography className="card__tag card__tag-single" sx={{ width: '87px' }}>
+            {tag}
+          </Typography>
+        ) : (
+          <div className="card__tags">
+            {tag.map((tag, index) => (
+              <Typography key={index} className="card__tag" sx={{ marginRight: '5px' }}>
+                {tag}
+              </Typography>
+            ))}
+          </div>
+        )}
         <Typography className="card__name">{name}</Typography>
         <Typography className="card__social">{social}</Typography>
         <Typography className="card__link">{link}</Typography>
@@ -26,7 +67,7 @@ export default function ContentCard({ tag, name, social, link, date, count }: Co
           <Typography className="card__date">{date}</Typography>
           <div className="card__done">
             <CardMedia component="img" image={doneIcon} alt="Done Icon" className="card__icon" />
-            <Typography className="card__count">{count}</Typography>
+            <Typography className={`card__count ${countColorClass}`}>{count}</Typography>
           </div>
         </div>
       </CardContent>
