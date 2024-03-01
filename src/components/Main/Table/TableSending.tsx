31a2b6fcdcc5a04524326f352cbe4b40d8738dd2
Nickolai <lindeman072@mail.ru>
2024-03-01 23:52:46 +0400

    add: dropdawn

--- src/components/Main/Table/TableSending.tsx
+++ src/components/Main/Table/TableSending.tsx
@@ -1,33 +1,14 @@
-import React, { useState } from "react";
 import "./TableSending.css";
 import ArrowDown from "../../../assets/Arrow_down.svg";
 import ArrowUp from "../../../assets/Arrow_up.svg";
+import { useState } from "react";
 
 export default function DataTable() {
-  const initialData = [
-    {
-      name: "Мерч",
-      merch: "Силект",
-      clothe: "Силект",
-      socks: "Силект",
-      date: "23.02.2024",
-      curator: "Имя куратора",
-      index: "154178",
-    },
-    // Добавьте другие элементы в массив данных по мере необходимости
-  ];
+  const [isArrowUp, setIsArrowUp] = useState(false);
 
-  const [dropdownStates, setDropdownStates] = useState(
-    initialData.map(() => false)
-  );
-
-  const toggleDropdown = (index) => {
-    const updatedDropdownStates = dropdownStates.map((state, idx) =>
-      idx === index ? !state : false
-    );
-    setDropdownStates(updatedDropdownStates);
+  const handleArrowClick = () => {
+    setIsArrowUp(!isArrowUp);
   };
-
   return (
     <div className="table__sending">
       <div className="table__container-header">
@@ -41,57 +22,96 @@ export default function DataTable() {
         <div className="table__index table__th_size_m">Индекс</div>
         <div className="table__th_size_s"></div>
       </div>
-      {initialData.map((item, index) => (
-        <div key={index} className="table__container-data">
-          <div className="table__check table__th_size_s"></div>
-          <div className="table__name table__th_size_xl">Мерч</div>
-          <div className="table__merch table__th_size_x">Силект</div>
-          <div className="table__clothe table__th_size_x">Силект</div>
-          <div className="table__socks table__th_size_x">Силект</div>
-          <div className="table__date table__th_size_x">23.02.2024</div>
-          <div className="table__curator table__th_size_l">Имя куратора</div>
-          <div className="table__index table__th_size_m">154178</div>
-          <div
-            className="table__dropdawn table__th_size_s"
-            onClick={() => toggleDropdown(index)}
-          >
-            {dropdownStates[index] ? (
-              <img src={ArrowUp} alt="иконка стрелочки" />
-            ) : (
-              <img src={ArrowDown} alt="иконка стрелочки" />
-            )}
-          </div>
-          
-          {dropdownStates[index] && (
-            <div className="table__container-dropdawn">
-              <div className="table__container-dropdawn-item table__td_size_s">
-                <p className="table__title">Страна</p>
-                <p className="table__subtitle">Россия</p>
-              </div>
-              <div className="table__container-dropdawn-item table__td_size_l">
-                <p className="table__title">Город</p>
-                <p className="table__subtitle">Москва</p>
-              </div>
-              <div className="table__container-dropdawn-item table__td_size_xl">
-                <p className="table__title">Улица, дом</p>
-                <p className="table__subtitle">Пионерская 43, дом 15</p>
-              </div>
-              <div className="table__container-dropdawn-item table__td_size_x">
-                <p className="table__title">Телефон</p>
-                <p className="table__subtitle">+791234567891</p>
-              </div>
-              <div className="table__container-dropdawn-item table__td_size_s">
-                <p className="table__title">Месяц</p>
-                <p className="table__subtitle">Февраль</p>
-              </div>
-              <div className="table__container-dropdawn-item table__td_size_xxl">
-                <p className="table__title">Комментарий</p>
-                <p className="table__subtitle">Инпут</p>
-              </div>{" "}
-            </div>
+      <div className="table__container-data">
+        <div className="table__check table__th_size_s"></div>
+        <div className="table__name table__th_size_xl">Мерч</div>
+        <div className="table__merch table__th_size_x">Силект</div>
+        <div className="table__clothe table__th_size_x">Силект</div>
+        <div className="table__socks table__th_size_x">Силект</div>
+        <div className="table__date table__th_size_x">23.02.2024</div>
+        <div className="table__curator table__th_size_l">Имя куратора</div>
+        <div className="table__index table__th_size_m">154178</div>
+        <div className="table__dropdawn table__th_size_s" onClick={handleArrowClick}> 
+          {isArrowUp ? (
+            <img src={ArrowUp} alt="иконка стрелочки" />
+          ) : (
+            <img src={ArrowDown} alt="иконка стрелочки" />
           )}
+       </div>
         </div>
-      ))}
+        {isArrowUp && (
+        <div className="table__container-dropdawn">
+          <div className="table__container-dropdawn-item table__td_size_s">
+            <p className="table__title">Страна</p>
+            <p className="table__subtitle">Россия</p>
+          </div>
+          <div className="table__container-dropdawn-item table__td_size_l">
+            <p className="table__title">Город</p>
+            <p className="table__subtitle">Москва</p>
+          </div>
+          <div className="table__container-dropdawn-item table__td_size_xl">
+            <p className="table__title">Улица, дом</p>
+            <p className="table__subtitle">Пионерская 43, дом 15</p>
+          </div>
+          <div className="table__container-dropdawn-item table__td_size_x">
+            <p className="table__title">Телефон</p>
+            <p className="table__subtitle">+791234567891</p>
+          </div>
+          <div className="table__container-dropdawn-item table__td_size_s">
+            <p className="table__title">Месяц</p>
+            <p className="table__subtitle">Февраль</p>
+          </div>
+          <div className="table__container-dropdawn-item table__td_size_xxl">
+            <p className="table__title">Комментарий</p>
+            <p className="table__subtitle">Инпут</p>
+          </div>{" "}
+              </div>
+         ) }
+          <div className="table__container-data">
+        <div className="table__check table__th_size_s"></div>
+        <div className="table__name table__th_size_xl">Мерч</div>
+        <div className="table__merch table__th_size_x">Силект</div>
+        <div className="table__clothe table__th_size_x">Силект</div>
+        <div className="table__socks table__th_size_x">Силект</div>
+        <div className="table__date table__th_size_x">23.02.2024</div>
+        <div className="table__curator table__th_size_l">Имя куратора</div>
+        <div className="table__index table__th_size_m">154178</div>
+        <div className="table__dropdawn table__th_size_s" onClick={handleArrowClick}> 
+          {isArrowUp ? (
+            <img src={ArrowUp} alt="иконка стрелочки" />
+          ) : (
+            <img src={ArrowDown} alt="иконка стрелочки" />
+          )}
+       </div>
+        </div>
+        {isArrowUp && (
+        <div className="table__container-dropdawn">
+          <div className="table__container-dropdawn-item table__td_size_s">
+            <p className="table__title">Страна</p>
+            <p className="table__subtitle">Россия</p>
+          </div>
+          <div className="table__container-dropdawn-item table__td_size_l">
+            <p className="table__title">Город</p>
+            <p className="table__subtitle">Москва</p>
+          </div>
+          <div className="table__container-dropdawn-item table__td_size_xl">
+            <p className="table__title">Улица, дом</p>
+            <p className="table__subtitle">Пионерская 43, дом 15</p>
+          </div>
+          <div className="table__container-dropdawn-item table__td_size_x">
+            <p className="table__title">Телефон</p>
+            <p className="table__subtitle">+791234567891</p>
+          </div>
+          <div className="table__container-dropdawn-item table__td_size_s">
+            <p className="table__title">Месяц</p>
+            <p className="table__subtitle">Февраль</p>
+          </div>
+          <div className="table__container-dropdawn-item table__td_size_xxl">
+            <p className="table__title">Комментарий</p>
+            <p className="table__subtitle">Инпут</p>
+          </div>{" "}
+              </div>
+         ) }
     </div>
-  );
+    );
 }
