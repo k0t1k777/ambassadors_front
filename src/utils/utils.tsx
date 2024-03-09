d836320c81b1bd4526df58f200aeecd3d9808695
Olga Pankrashina <olyaolya2713@yandex.ru>
2024-03-09 12:44:49 +0300

    ref: old change

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,6 +1,6 @@
 // export const BASE_URL = "http://localhost:8000";
 export const BASE_URL = 'http://5.35.89.44:8000';
-export const TOKEN = '85f4eb5d5a8b44cfd13b6af1b23721d03a1ca826';
+export const TOKEN = '96df25d91efe73db6facf45407ca6327fb26d4d3';
 
 const headers = {
   authorization: `Token ${TOKEN}`,
